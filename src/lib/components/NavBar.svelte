6ce5070fad5e7f8fdb9809d560df03f3053e1f0c
Elena Lungoci <simona.lungoci@yahoo.com>
2024-12-22 18:48:14 -0500

    Add Nav Bar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,60 @@
+<script>
+</script>
+
+<div>
+  <!-- add color change on hover -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark font-inter">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="./">
+        <div class="container">
+          <div class="row">
+            <div
+              class="turquoise dot col-sm text-dark text-center font-kablammo icon"
+            >
+              E
+            </div>
+            <div class="col-sm">Elena Lungoci</div>
+          </div>
+        </div>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item me-2 ms-2">
+          <a class="nav-link active" href="./">Skills</a>
+        </li>
+        <li class="nav-item me-2 ms-2">
+          <a class="nav-link active" href="./">Projects</a>
+        </li>
+        <li class="nav-item me-2 ms-2">
+          <a class="nav-link active" href="./">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</div>
+
+<style>
+  .dot {
+    height: 35px;
+    width: 35px;
+    border-radius: 50%;
+    display: inline-block;
+  }
+  .icon {
+    text-align: center;
+    vertical-align: middle;
+    line-height: 35px;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Page from "./+page.svelte";
+  import NavBar from "$lib/components/NavBar.svelte";
+  import '$lib/global.css';
+</script>
+
+<NavBar />
+
+<slot />
